347f09e3ea1a79c06695e857f819ced591a3d933
behzod <behzod4214974@gmail.com>
2023-11-19 21:34:13 +0500

    js and styles were added

---
+++ js/main.js
@@ -0,0 +1,99 @@
+// ! utils 
+const getElement = (element , parentElement = document) => parentElement.querySelector(element)
+
+const fixDate = (time) =>{
+	let newDate = new Date(time)
+	let fixedDate = ''
+	fixedDate += newDate.getDate()
+	fixedDate += `.${String(newDate.getMonth() + 1).padStart(2,'0')}.`
+	fixedDate += newDate.getFullYear()
+	return fixedDate
+}
+
+const createBenefitItem = (benefit, goingElement) =>{
+	const newBenefit = document.createElement('li')
+	newBenefit.className = 'badge bg-primary me-1 mb-1'
+	newBenefit.textContent = benefit
+	goingElement.append(newBenefit)
+}
+
+// ! variables for dom node
+const  elTemplate  =  getElement('.template').content
+const  elProductList  =  getElement('.card-list')
+const elForm = getElement('.modal-form')
+const elModalSelect = getElement('#product-manufacturer')
+const elManufactureSelect = getElement('.form-select')
+const  $productCount = getElement('.count')
+
+// ! rendering manufacture filter
+const renderManufacturers = (manufacturers, goingElement) =>{
+	goingElement.innerHTML = null
+	goingElement.innerHTML = ' <option value="0">All</option>'
+
+	const selectFragment = document.createDocumentFragment()
+	manufacturers.forEach(product => {
+		const newOption = document.createElement('option')
+		newOption.textContent = product.name
+		newOption.value = product.id
+		selectFragment.append(newOption)
+	})
+	goingElement.append(selectFragment)
+} 
+
+renderManufacturers(manufacturers,elManufactureSelect)
+
+// ! creating modal select option
+
+const createModalOption = (manufacturers, goingElement ) =>{
+	goingElement.innerHTML = null 
+	goingElement.innerHTML = ' <option  selected disabled value="0">Select a manufacturer</option>'
+	
+	const modalFragment = document.createDocumentFragment()
+	manufacturers.forEach(product => {
+		const modalOption = document.createElement('option')
+		modalOption.value = product.id
+		modalOption.textContent = product.name
+		modalFragment.append(modalOption)
+	})
+	goingElement.append(modalFragment)
+}
+
+createModalOption(manufacturers,elModalSelect)
+
+// ! rendering product card
+const renderProduct = (products, goingElement) => {
+	goingElement.innerHTML = null 
+	const fragment  = document.createDocumentFragment()
+	// count 
+	$productCount.textContent = `count: ${products.length}`
+	
+	products.forEach(product => {
+		const {id,title,img,price,model,addedDate,benefits} = product
+		const template = elTemplate.cloneNode(true)
+		
+		getElement('.card-list__item',template).id = id
+		template.querySelector('.card-img-top').src = img
+		template.querySelector('.card-title').textContent = title
+		template.querySelector('.card-text').textContent = price.toLocaleString('es-ES', { style: 'currency', currency: 'UZS' });
+		template.querySelector('.product-name').textContent = model
+		template.querySelector('.product-date').textContent = fixDate(addedDate)
+		benefits.forEach(benefit => {
+			createBenefitItem(benefit,template.querySelector('.benefit-list'))
+		})
+		
+		fragment.append(template)
+	});
+	
+	goingElement.append(fragment)
+}
+
+renderProduct(products,elProductList)
+
+
+const handleModal = (e) => {
+		e.preventDefault()
+		
+		e.target.reset()
+}
+
+elForm.addEventListener('submit', handleModal)
