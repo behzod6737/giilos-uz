94a4d41eec57a8345a1deadec65719a47fde4500
behzod <behzod4214974@gmail.com>
2023-11-21 17:45:44 +0500

    filter and add searches are done and btn is done

--- js/main.js
+++ js/main.js
@@ -11,14 +11,41 @@ const fixDate = (time) => {
   return fixedDate;
 };
 
-const createBenefitItem = (benefit, goingElement) => {
+const createBenefitItem = (benefit,  goingElement) => {
   const newBenefit = document.createElement("li");
-  newBenefit.className = "badge bg-primary me-1 mb-1";
+  newBenefit.className = "badge bg-primary me-1 mb-1"
   newBenefit.textContent = benefit;
   goingElement.append(newBenefit);
 };
 
+
+const addNewBenefit = (benefit, goingElement) => {
+  const newBenefit = document.createElement("li");
+  const newBenefitBtn = document.createElement('button')
+  newBenefit.className = 'me-1 mb-1'
+  newBenefitBtn.className = 'btn btn-sm badge rounded-pill btn-danger'
+  newBenefitBtn.textContent = benefit ;
+  newBenefit.append(newBenefitBtn)
+  goingElement.append(newBenefit);
+};
+
 // ! variables for dom node
+const elFormFilter = getElement('.filter-form')
+const elInputFilterName = getElement('.input-name__filter')
+
+const elFormAdd = getElement('.add-form')
+const  elFormAddName = getElement('.form-add-name')
+const  elFormAddPrice = getElement('.form-add-price')
+const  elFormAddBenefits = getElement('.form-add-benefits')
+const elFormAddBenefitList =getElement('.benefits-list')
+
+const elEditForm = getElement('.form-edit-modal')
+const elEditFormName = getElement('.form-edit-name')
+const elEditFormPrice = getElement('.form-edit-price')
+const elEditFormSelect = getElement('.form-edit-select')
+const elEditFormBenefit= getElement('.form-edit-benefit')
+
+
 const elTemplate = getElement(".template").content;
 const elProductList = getElement(".card-list");
 const elForm = getElement(".modal-form");
@@ -53,8 +80,9 @@ const createModalOption = (manufacturers, goingElement) => {
   const modalFragment = document.createDocumentFragment();
   manufacturers.forEach((product) => {
     const modalOption = document.createElement("option");
-    modalOption.value = product.id;
-    modalOption.textContent = product.name;
+    modalOption.id = product.id;
+	modalOption.value = product.name;
+    modalOption.textContent = product.name.toUpperCase();
     modalFragment.append(modalOption);
   });
   goingElement.append(modalFragment);
@@ -77,9 +105,7 @@ const renderProduct = (products, goingElement) => {
     template.querySelector(".card-img-top").src = img;
     template.querySelector(".card-title").textContent = title;
     template.querySelector(".card-text").textContent = price.toLocaleString(
-      "es-ES",
-      { style: "currency", currency: "UZS" }
-    );
+      "es-ES", { style: "currency", currency: "UZS" });
     template.querySelector(".product-name").textContent = model;
     template.querySelector(".product-date").textContent = fixDate(addedDate);
     benefits.forEach((benefit) => {
@@ -94,24 +120,99 @@ const renderProduct = (products, goingElement) => {
 
 renderProduct(products, elProductList);
 
-// const handleModal = (e) => {
-// 		e.preventDefault()
+// ! filter product cards 
 
-// 		e.target.reset()
-// }
-
-// ! delete card function
-const deleteCard = (event) => {
-	if (event.target.matches('.btn__delete')) {
-		const cardId = event.target.closest('.card-list__item').dataset.id
-		const currentCard = products.findIndex(product => product.id === +cardId)
-		products.splice(currentCard,1)
-		renderProduct(products, elProductList);
-		}
-};
-
-if (elProductList) {
-  elProductList.addEventListener("click", deleteCard);
+const filterProducts = (event) => {
+	event.preventDefault()
+	
+	if (elInputFilterName.value.trim() === '') {
+	 return	renderProduct(products, elProductList);
+	}
+	const regExp = new RegExp( elInputFilterName.value.trim(), 'gi')
+	renderProduct(products.filter(product => product.title.match(regExp)), elProductList)
+	event.target.reset()
+	//  renderProduct(products.filter(product => product.title.toLowerCase().includes(elInputFilterName.value.toLowerCase().trim()), elProductList));
+	
 }
 
-// elForm.addEventListener("submit", handleModal);
+
+// ! add product cards
+const addProductCard = (event) => {
+	event.preventDefault()
+
+	if (elFormAddName.value.trim()) {
+	let [{img, ...rest}] = products
+	let manufacturerNames = manufacturers.map(manufacturer => manufacturer.name)
+	let selectedModal = manufacturerNames.filter(names =>  names.includes(elModalSelect.value))
+	let allBenefits = elFormAddBenefits.value.split(',')
+	
+	allBenefits.forEach(benefit => {
+		addNewBenefit(benefit, elFormAddBenefitList);
+	})
+
+	products.push({
+		id: products[products.length -1].id +1,
+		img,
+		title:elFormAddName.value,
+		model: selectedModal,
+		addedDate:new Date().toISOString(),
+		price:+elFormAddPrice.value,
+		benefits:allBenefits
+	});
+		renderProduct(products, elProductList);
+
+	}
+
+
+	alert('rostan ham yangi product qoshmoqchimisiz?')
+	event.target.reset()
+}
+
+// !edit and delete product
+
+const editDeleteFunc = (e) => {
+	if (e.target.matches('.btn__delete')) {
+		const productId = e.target.closest('.card-list__item').dataset.id 
+		const currentProduct = products.findIndex(product => product.id === +productId)
+		products.splice(currentProduct,1)
+		renderProduct(products, elProductList);
+
+	}else if(e.target.matches('.btn-secondary')) {
+		const productId = e.target.closest('.card-list__item').dataset.id 
+
+		const currentProductIndex = products.findIndex(product => product.id === +productId)
+
+		const {id,title,img,price,model,addedDate,benefits}= products[currentProductIndex]
+		
+		createModalOption(manufacturers,elEditFormSelect)
+
+		elEditFormName.value = title
+		elEditFormPrice.value = price
+		elEditFormSelect.value = model
+		elEditFormBenefit.value = benefits
+
+		elEditForm.addEventListener('submit' , (e) => {
+			e.preventDefault()
+		
+			if (elEditFormName.value.trim() && elEditFormPrice.value.trim()) {
+				const arrBenefits = elEditFormBenefit.value.split(',')
+				const editedProduct = {
+					id,
+					title:elEditFormName.value,
+					img,
+					price:+elEditFormPrice.value,
+					model:elEditFormSelect.value,
+					addedDate,
+					benefits:arrBenefits
+				}
+				products.splice(currentProductIndex,1,editedProduct)
+				renderProduct(products, elProductList);
+			}
+		})
+	}
+}
+
+
+elProductList.addEventListener('click', editDeleteFunc)
+elFormAdd.addEventListener('submit' , addProductCard )
+elFormFilter.addEventListener('submit' , filterProducts)
